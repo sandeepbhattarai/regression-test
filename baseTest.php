ec20dd5472304f9e0e5361bc7f3d41256c16e1ce
sandeepbhattarai <sandpbhatrai.is@gmail.com>
2018-06-07 16:10:07 +1000

    changed the base test content
    ch

--- baseTest.php
+++ baseTest.php
@@ -1,22 +1,30 @@
 <?php
-namespace Facebook\WebDriver;
-
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-require_once ('vendor/autoload.php');
-
-class baseTest
+class fileGrabber
 {
-
-    protected $driver;
-
-    public function __construct()
+   
+    
+    public function readFile()
     {
-        $host = 'http://localhost:4444/wd/hub'; // this is the default
-        $capabilities = DesiredCapabilities::chrome();
-        $this->driver = RemoteWebDriver::create($host, $capabilities, 5000);
+        $row = 1;
+        if (($handle = fopen("test.csv", "r")) !== FALSE) 
+        {
+            while (($data = fgetcsv($handle, 0, ",")) !== FALSE) 
+            {
+                $num = count($data);
+                if ($num > 3)
+                {
+                echo "improper data format";
+                exit();
+                }
+                $row++;
+                for ($c=0; $c < $num; $c++) {
+                    echo $data[$c] . " ";
+                }
+                echo "\n";
+            }
+            fclose($handle);
+        }
     }
 }
 
 ?>
-
